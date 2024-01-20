4172e52f8b521617fa95bee37c0db10449bd3a1e
Germline <priitsyld@gmail.com>
2024-01-20 09:59:54 +0200

    Add CSS styling and update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,21 @@ module.exports = {
     compress: true,
     port: 9000,
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
